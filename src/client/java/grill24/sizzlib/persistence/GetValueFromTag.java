63d7c2458b517b691f0792435fa3772661a43282
Curren Jeandell <curren.jeandell@king.com>
2023-12-23 20:21:42 +0000

    Remove unused

--- src/client/java/grill24/sizzlib/persistence/GetValueFromTag.java
+++
@@ -1,8 +0,0 @@
-package grill24.sizzlib.persistence;
-
-import net.minecraft.nbt.NbtCompound;
-
-@FunctionalInterface
-public interface GetValueFromTag {
-    Object get(NbtCompound tag, String key);
-}
--- src/client/java/grill24/sizzlib/persistence/SetValueInTag.java
+++
@@ -1,8 +0,0 @@
-package grill24.sizzlib.persistence;
-
-import net.minecraft.nbt.NbtCompound;
-
-@FunctionalInterface
-public interface SetValueInTag {
-    void set(NbtCompound tag, String key, Object value);
-}
